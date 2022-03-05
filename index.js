84fb33cb99c9672d447aff49db7275d2de25cb3d
Zach Lefler <leaving.valhalla@gmail.com>
2022-03-05 11:10:06 -0800

    commit tree 49a1a5e702c3463c25dcb503fb09358a1203d721

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function hasTargetSum(array, target) {
-  const seenNumbers = {};
+  const seenNumbers = new Set();
   for (const num of array) {
     const complement = target - num;
     console.log(seenNumbers);
-    if (seenNumbers[complement]) return true;
-    seenNumbers[num] = true;
+    if (seenNumbers.has(complement)) return true;
+    seenNumbers.add(num);
   }
   return false;
 }
@@ -25,7 +25,7 @@ function hasTargetSum(array, target) {
 if (require.main === module) {
   // add your own custom tests in here
   console.log('Expecting: true');
-  console.log('=>', hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+  console.log('=>', hasTargetSum([3, 3, 8, 12, 4, 11, 7], 10));
 
   console.log('');
 
